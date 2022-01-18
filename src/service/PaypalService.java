098b6038cfba6411156e8b83f256f1c691923d06
Wesly <weslyramalho@yahoo.com.br>
2022-01-18 16:55:25 -0300

    foi finalizado a criação da interface OnlinePaymentService

--- src/service/PaypalService.java
+++ src/service/PaypalService.java
@@ -5,10 +5,10 @@ public class PaypalService implements OnlinePaymentService {
 	private static final double FEE_PERCENTAGE = 0.02;
 	private static final double MONTHLY_INTEREST = 0.01;
 	
-	private Double paymentFee(Double amount) {
+	private Double paymentFee(double amount) {
 		return amount * FEE_PERCENTAGE;
 	}
-	private Double interest(Double amount, Integer months) {
+	private Double interest(double amount, Integer months) {
 		return amount * MONTHLY_INTEREST * months; 
 	}
 
