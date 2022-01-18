16815d1148028da086205c38d4c6db97edf0c0d0
Wesly <weslyramalho@yahoo.com.br>
2022-01-18 17:51:26 -0300

    logica da classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Contract;
+import entities.Installment;
+import service.ContractService;
+import service.PaypalService;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Enter contract data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Date (dd/MM/yyyy): ");
+		Date date = (Date) sdf.parse(sc.next());
+		System.out.print("Contract value: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract contract = new Contract(number, date, totalValue);
+		
+		System.out.print("Enter number of installments: ");
+		int n = sc.nextInt();
+		
+		ContractService contractService = new ContractService(new PaypalService());
+		
+		contractService.processContract(contract, n);
+		
+		System.out.println("Installments:");
+		for (Installment x : contract.getInstallments()) {
+			System.out.println(x);
+		}
+		
+		sc.close();
 
 	}
 
--- src/service/ContractService.java
+++ src/service/ContractService.java
@@ -10,6 +10,10 @@ public class ContractService {
 	
 	private OnlinePaymentService onlinePaymentService;
 	
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+
 	public void processContract(Contract contract, int months) {
 		double basicQuota = contract.getTotalValue() / months;
         for (int i = 1; i <= months; i++) {
