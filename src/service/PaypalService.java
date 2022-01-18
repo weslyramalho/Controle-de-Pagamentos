13721bedfa503fb8ac09f418a8bab5ac53fec07e
Wesly <weslyramalho@yahoo.com.br>
2022-01-18 16:50:58 -0300

    Foi criado as class da pasta service

---
+++ src/service/PaypalService.java
@@ -0,0 +1,17 @@
+package service;
+
+public class PaypalService implements OnlinePaymentService {
+	
+	private static final double FEE_PERCENTAGE = 0.02;
+	private static final double MONTHLY_INTEREST = 0.01;
+	
+	private Double paymentFee(Double amount) {
+		return amount * FEE_PERCENTAGE;
+	}
+	private Double interest(Double amount, Integer months) {
+		return amount * MONTHLY_INTEREST * months; 
+	}
+
+	
+
+}
